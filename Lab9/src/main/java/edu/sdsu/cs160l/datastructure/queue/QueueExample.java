b36dd544cfc605e8bebb9ed3ba8a31cfbab1847a
bb1177 <bbrady1177@sdsu.edu>
2026-04-13 23:52:58 -0700

    Update QueueExample.java

--- Lab9/src/main/java/edu/sdsu/cs160l/datastructure/queue/QueueExample.java
+++ Lab9/src/main/java/edu/sdsu/cs160l/datastructure/queue/QueueExample.java
@@ -45,6 +45,31 @@ public class QueueExample {
         // TODO - remove the first two students and add 1 student
         // TODO - print the final queue
 
+        Queue<Student> studentQueue = new LinkedList<>();
+
+        Student s1 = new Student(1L, "John", 4.0, StudentLevel.FRESHMAN, StudentMajor.COMPUTER_SCIENCE);
+        Student s2 = new Student(2L, "Jane", 3.8, StudentLevel.SOPHOMORE, StudentMajor.COMPUTER_ENGINEERING);
+        Student s3 = new Student(3L, "Mike", 3.5, StudentLevel.JUNIOR, StudentMajor.COMPUTER_SCIENCE);
+        Student s4 = new Student(4L, "Sara", 3.9, StudentLevel.SENIOR, StudentMajor.COMPUTER_ENGINEERING);
+        Student s5 = new Student(5L, "Alex", 3.7, StudentLevel.FRESHMAN, StudentMajor.COMPUTER_SCIENCE);
+        
+        studentQueue.add(s1);
+        studentQueue.add(s2);
+        studentQueue.add(s3);
+        studentQueue.add(s4);
+        studentQueue.add(s5);
+        
+        studentQueue.poll();
+        studentQueue.poll();
+        
+        Student s6 = new Student(6L, "Chris", 3.6, StudentLevel.JUNIOR, StudentMajor.COMPUTER_ENGINEERING);
+        studentQueue.add(s6);
+        
+        while (!studentQueue.isEmpty()) {
+            Student s = studentQueue.poll();
+            System.out.println(s.getRedId() + " " + s.getName());
+        }
+
         Student john = new Student(1L, "John", 4.0, StudentLevel.FRESHMAN, StudentMajor.COMPUTER_SCIENCE);
     }
 }
